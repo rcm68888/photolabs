4aa990f3e13b8fb1e3e65bd6d9688e4d9907b56c
Rcm <rcm68888@gmail.com>
2024-04-02 15:51:11 -0600

    Added the top nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
 import TopicList from './components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
+import FavBadge from 'components/FavBadge';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -8,6 +10,10 @@ const App = () => {
   return (
     <div className="App">
       <TopicList />
+      <TopNavigation>
+        <TopicList />
+        <FavBadge />
+      </TopNavigation>
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      {props.children}
     </div>
   )
 }
