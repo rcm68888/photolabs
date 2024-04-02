45c4750f01299db1525da60a52847f836c2e402c
Rcm <rcm68888@gmail.com>
2024-04-02 07:59:46 -0600

    Added the function to display 3 pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-//import PhotoListItem from './components/PhotoListItem';
+import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
-import './App.scss';
+//import './App.scss';
+import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
+const sampleDataForPhotoListItem = [{
   id: "1",
   location: {
     city: "Montreal",
@@ -13,7 +14,25 @@ const sampleDataForPhotoListItem = {
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+}, {
+  id: "2",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+}, {
+  id: "3",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+}];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
