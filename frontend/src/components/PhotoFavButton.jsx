b3de377679ffac64678fa943848109287e3699a8
Rcm <rcm68888@gmail.com>
2024-04-02 09:16:37 -0600

    Added the favourite Icon functionity

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,11 +4,17 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [selected, setSelected] = useState(false);
+  const handleClick = useCallback(()=>{
+    setSelected(!selected);
+  })
   return (
     <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} />
       </div>
+    </div> 
     </div>
   );
 }
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
     <li  className="photo-list__item" >
-      <img className="photo-list__image"  src={props.data.urls.regular} onClick={handleClick}></img>
+      <PhotoFavButton/>
+      <img className="photo-list__image"  src={props.data.imageSource}></img>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.data.user.profile}></img>
+        <img className="photo-list__user-profile" src={props.data.profile}> </img>
         <div className="photo-list__user-info">
-          <p>{props.data.user.username}</p>
+          <p>{props.data.username}</p>
           <p className="photo-list__user-location">
             <span>{props.data.location.city}</span>, <span>{props.data.location.country}</span>
           </p>
