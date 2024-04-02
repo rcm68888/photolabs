6e1a8219364cb4168d547c85651c4f188738ebbe
Rcm <rcm68888@gmail.com>
2024-04-02 16:09:05 -0600

    create the homeroute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
-import FavBadge from 'components/FavBadge';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
-      <TopNavigation>
-        <TopicList />
-        <FavBadge />
-      </TopNavigation>
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import PhotoList from './components/PhotoList';
+import TopicList from './components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
+import FavBadge from 'components/FavBadge';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation>
+        <TopicList />
+        <FavBadge />
+      </TopNavigation>
+      <PhotoList />
     </div>
   );
 };
