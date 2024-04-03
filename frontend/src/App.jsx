679090b401de099db429d8687935aba45f370f18
Rcm <rcm68888@gmail.com>
2024-04-03 07:45:17 -0600

    added the side peak model component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [modal, setModal ] = useState(false);
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} />
+      <HomeRoute photos={photos} topics={topics} setModal={setModal} modal={modal}/>
+      {modal && <PhotoDetailsModal setModal={setModal} modal={modal} />}
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const handleClick = () =>{
+    props.setModal(!props.modal);
+  }
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={handleClick}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
