c42efd8c68c468271f17fde19e02df61205a282a
Rcm <rcm68888@gmail.com>
2024-04-03 07:08:01 -0600

    added the display Notif for Fav Photos component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,14 +4,29 @@ import TopNavigation from 'components/TopNavigationBar';
 import TopicList from 'components/TopicList';
 import PhotoList from 'components/PhotoList';
 import FavBadge from 'components/FavBadge';
+import photos from 'mocks/photos';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
+  const stateObject = photos.reduce((result, _, index) => {
+    result[index] = false;
+    return result;
+  }, {});
+
+  const [state, dispatch] = useReducer((state, action)=>{
+    switch(action.type){
+      case "FAVOURITE":
+      return state;
+    }
+  },stateObject);
+
+  const isFavPhotoExist = Object.values(state).includes(true);
+
   return (
     <div className="home-route">
       <TopNavigation>
         <TopicList topics={props.topics}/>
-        <FavBadge />
+        <FavBadge isFavPhotoExist={isFavPhotoExist}/>
       </TopNavigation>
       <PhotoList photos={props.photos} dispatch={dispatch} state={state}/>
     </div>
